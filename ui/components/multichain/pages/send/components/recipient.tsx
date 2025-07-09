e0bfb5d261d0f35a134a947383f825fe66924ca7
893281878@qq.com <893281878@qq.com>
2025-07-09 12:00:14 +0800

    fix bug

--- ui/components/multichain/pages/send/components/recipient.tsx
+++ ui/components/multichain/pages/send/components/recipient.tsx
@@ -123,17 +123,18 @@ export const SendPageRecipient = () => {
     contents = (
       <Tabs
         defaultActiveTabKey={
-          userInput.length > 0 ? CONTACTS_TAB_KEY : ACCOUNTS_TAB_KEY
+          CONTACTS_TAB_KEY
+          // userInput.length > 0 ? CONTACTS_TAB_KEY : ACCOUNTS_TAB_KEY
         }
         onTabClick={() => null}
       >
-        {
+        {/* {
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
           <Tab tabKey={ACCOUNTS_TAB_KEY} name={t('yourAccounts')}>
             <SendPageYourAccounts />
           </Tab>
-        }
+        } */}
         {
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
