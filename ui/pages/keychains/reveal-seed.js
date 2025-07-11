9f588fab376b1ac5178180af54a236730d440a72
893281878@qq.com <893281878@qq.com>
2025-07-12 00:20:28 +0800

    fix bug

--- ui/pages/keychains/reveal-seed.js
+++ ui/pages/keychains/reveal-seed.js
@@ -364,8 +364,8 @@ export default function RevealSeedPage() {
             variant={BUTTON_VARIANT.LINK}
             size={BUTTON_SIZES.INHERIT}
             as="a"
-            href={ZENDESK_URLS.SECRET_RECOVERY_PHRASE}
-            target="_blank"
+            // href={ZENDESK_URLS.SECRET_RECOVERY_PHRASE}
+            // target="_blank"
             rel="noopener noreferrer"
           >
             {t('revealSeedWordsSRPName')}
@@ -386,8 +386,8 @@ export default function RevealSeedPage() {
             variant={BUTTON_VARIANT.LINK}
             size={BUTTON_SIZES.INHERIT}
             as="a"
-            href={ZENDESK_URLS.NON_CUSTODIAL_WALLET}
-            target="_blank"
+            // href={ZENDESK_URLS.NON_CUSTODIAL_WALLET}
+            // target="_blank"
             rel="noopener noreferrer"
           >
             {t('revealSeedWordsNonCustodialWallet')}
